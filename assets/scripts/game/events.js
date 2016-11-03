0b21a71ce71e7b759240eaeeddd3f3a62673d8d7
kfili <kosta.filiotis@gmail.com>
2016-11-03 11:02:00 -0400

    designed basic structure of game logic
    wrote functions that switch turns, set board values,
    and functions that check for clicks, win conditions.

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -1,48 +1,95 @@
 'use strict';
 
-const store = require('../store');
+// const store = require('../store');
 
+const board = [
+  '', '', '',
+  '', '', '',
+  '', '', ''
+];
 
+let turn = 'x';
+
+const switchTurn = function () {
+  if (turn === 'x') {
+    turn = 'o';
+  } else {
+      turn = 'x';
+  }
+  // board[index] = turn;
+};
 
 //--single cell was clicked - do something
 // change background image in cell.
 // set owner?
 
-const onCLICKtl = function(event){
-  event.preventDefault();
-  if (store.tl= ) {
-
-  }
+const isWinX = function () {
+  if(board[0] === 'x' && board[1] === 'x' && board[2] === 'x'
+  )
+  {console.log('win');
+} else {
+  console.log('nope');
+}
 };
+
+const setBoard = function (e) {
+  e.preventDefault();
+  if (board[$(this).data('cell')] !== '') {
+    board[$(this).data('cell')] = turn;
+  }
+  //run isWin
+  //isWin()
+  //update turn status
+  switchTurn();
+  //to X/O depending on (turnstatus)
+  console.log(board[0]);
+  debugger;
+};
+
+// const onCLICKtl = function(event){
+//   event.preventDefault();
+//   if (store.tl= ) {
+//
+//   }
+// };
 
 //--all cells were clicked - do something
-const cellsClick = function(){
-  event.preventDefault();
-  $(' ').data(this)
-  debugger;
-  // if (store.tl= ) {
-  //
-  // }
-};
+// const cellsClick = function(){
+//   event.preventDefault();
+//   $(' ').data(this)
+//   debugger;
+//   if (store.tl= ) {
+//
+//   }
+// };
 
-//check for horizontal wins
+//check for wins & whos turn is it?
 
-//check for vertical wins
-
-//check for draws
 
 //--Click handler
 const gameHandlers = () => {
-  $('.col-xs-4').on('click', cellsClick);
-  $('#tc').on('click', onCLICKtc);
-  $('#tr').on('click', onCLICKtr);
-  $('#ml').on('click', onCLICKml);
-  $('#mc').on('click', on);
-  $('#mr').on('click', onClickTL);
-  $('#bl').on('click', onClickTL);
-  $('#bc').on('click', onClickTL);
-  $('#br').on('click', onClickTL);
+  $('.col-xs-4').on('click', setBoard);
+  // $('#tc').on('click', onCLICKtc);
+  // $('#tr').on('click', onCLICKtr);
+  // $('#ml').on('click', onCLICKml);
+  // $('#mc').on('click', onCLICKmc);
+  // $('#mr').on('click', onCLICKmr);
+  // $('#ml').on('click', onCLICKbl);
+  // $('#bc').on('click', onCLICKbc);
+  // $('#br').on('click', onCLICKbr);
 };
+//
+// const gameHandlers = () => {
+//   $('#tr').on('click', onCLICKtr);
+//   $('#tc').on('click', onCLICKtc);
+//   $('#tr').on('click', onCLICKtr);
+//   $('#ml').on('click', onCLICKml);
+//   $('#mc').on('click', onCLICKmc);
+//   $('#mr').on('click', onCLICKmr);
+//   $('#ml').on('click', onCLICKbl);
+//   $('#bc').on('click', onCLICKbc);
+//   $('#br').on('click', onCLICKbr);
+// };
 
 module.exports = {
   gameHandlers,
