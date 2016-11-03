97eefed0266fdbc6c5923b32ea5ed92ce543cb84
kfili <kosta.filiotis@gmail.com>
2016-11-03 19:54:14 -0400

    added detection and announcement of draw result

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -2,7 +2,7 @@
 
 // const store = require('../store');
 
-let over = false;
+let gState = 'go';
 let turn = 'X';
 const board = [
   '', '', '',
@@ -39,7 +39,7 @@ const isWin = function () {
     || board[2] === 'X' && board[4] === 'X' && board[6] === 'X'
       ) {
         console.log('X wins!!');
-        return true;
+        return 'winX';
       } else if (
     board[0] === 'O' && board[1] === 'O' && board[2] === 'O'
     || board[3] === 'O' && board[4] === 'O' && board[5] === 'O'
@@ -51,41 +51,44 @@ const isWin = function () {
     || board[2] === 'O' && board[4] === 'O' && board[6] === 'O'
       ) {
         console.log('O wins!!');
-        return true;
+        return 'winO';
       } else if (
     board[0] !== '' && board[1] !== '' && board[2] !== ''
     && board[3] !== '' && board[4] !== '' && board[5] !== ''
     && board[6] !== '' && board[7] !== '' && board[8] !== ''
       ) {
         console.log('CATs game');
-        return true;
+        return 'cat';
       } else {
         console.log('nada');
-        return false;
+        return 'go';
       }
 };
 
 const cellclick = function (e) {
   e.preventDefault();
-    if (over === false) {
+    if (gState === 'go') {
       if (board[$(this).data('cell')] === '' ) {
           board[$(this).data('cell')] = turn;
-          $(this).text(turn);                 // sets X/O to a div
-          over = isWin();         // returns true if win or draw state detected
-
-          if (over === true) {
-              console.log(over);
-              $('#status').text('Player X wins!!');
-              debugger;
+          $(this).text(turn);     // sets X/O to a div
+          gState = isWin();         // returns true if win or draw state detected
+          if (gState !== 'go') {
+              console.log(gState);
+              if (gState === 'winX') {
+                $('#status').text('Player X wins!!');
+              } else if (gState === 'winO') {
+                $('#status').text('Player O wins!!');
+              } else {
+                $('#status').text('Meeoow');
+              }
             } else {
-            console.log(over);
-            switchTurn();                       // switches a variable that = X or O
+            console.log(gState);
+            switchTurn();         // switches a variable that = X or O
             }
           }
-    } else if (over === true) {
-      debugger;
-      emptyBoard();                           // clears the board array.
-      over = false;
+    } else if (gState !== 'go') {
+      emptyBoard();               // clears the board array.
+      gState = 'go';
       switchTurn();
       }
   };
@@ -94,37 +97,8 @@ const gameHandlers = () => {
   $('.col-xs-4').on('click', cellclick);
   };
 
-  //cellFiller
-    // if cell full do nothing
-    // if cell empty check turn and fill
-    // call isWin?
-    // switchTurn?
+
 
 module.exports = {
   gameHandlers,
 };
-
-// const cellclick = function (e) {
-//   e.preventDefault();
-//   //check the game state
-//
-//
-//   //update board
-//   if (board[$(this).data('cell')] === '' && over === false) {
-//     board[$(this).data('cell')] = turn;
-//     $(this).text(turn); // sets X/O to a div
-//     let over = isWin();
-//
-//     if (over === 'winX') {
-//       debugger;
-//         $('#status').text('Player X wins!!');
-//         over = true;
-//     }
-//     switchTurn();
-//   } else if (over === true) {
-//     debugger
-//     emptyBoard();
-//   } else {
-//     console.log('this cell is taken, DND!');
-//   }
-// };
