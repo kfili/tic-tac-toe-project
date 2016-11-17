c211c8cdfde3cd20dbcc324d1dc578b86b9f7ef0
kfili <kosta.filiotis@gmail.com>
2016-11-17 17:25:22 -0500

    add game history modal

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -1,4 +1,4 @@
-'use strict';
+ 'use strict';
 
 // const store = require('../store');
 
@@ -29,33 +29,33 @@ const switchTurn = function () {
 
 const isWin = function () {
     if(
-    board[0] === 'X' && board[1] === 'X' && board[2] === 'X'
-    || board[3] === 'X' && board[4] === 'X' && board[5] === 'X'
-    || board[6] === 'X' && board[7] === 'X' && board[8] === 'X'
-    || board[0] === 'X' && board[3] === 'X' && board[6] === 'X'
-    || board[1] === 'X' && board[4] === 'X' && board[7] === 'X'
-    || board[2] === 'X' && board[5] === 'X' && board[8] === 'X'
-    || board[0] === 'X' && board[4] === 'X' && board[8] === 'X'
-    || board[2] === 'X' && board[4] === 'X' && board[6] === 'X'
+    board[0] === 'X' && board[1] === 'X' && board[2] === 'X' ||
+    board[3] === 'X' && board[4] === 'X' && board[5] === 'X' ||
+    board[6] === 'X' && board[7] === 'X' && board[8] === 'X' ||
+    board[0] === 'X' && board[3] === 'X' && board[6] === 'X' ||
+    board[1] === 'X' && board[4] === 'X' && board[7] === 'X' ||
+    board[2] === 'X' && board[5] === 'X' && board[8] === 'X' ||
+    board[0] === 'X' && board[4] === 'X' && board[8] === 'X' ||
+    board[2] === 'X' && board[4] === 'X' && board[6] === 'X'
       ) {
         console.log('X wins!!');
         return 'winX';
       } else if (
-    board[0] === 'O' && board[1] === 'O' && board[2] === 'O'
-    || board[3] === 'O' && board[4] === 'O' && board[5] === 'O'
-    || board[6] === 'O' && board[7] === 'O' && board[8] === 'O'
-    || board[0] === 'O' && board[3] === 'O' && board[6] === 'O'
-    || board[1] === 'O' && board[4] === 'O' && board[7] === 'O'
-    || board[2] === 'O' && board[5] === 'O' && board[8] === 'O'
-    || board[0] === 'O' && board[4] === 'O' && board[8] === 'O'
-    || board[2] === 'O' && board[4] === 'O' && board[6] === 'O'
+    board[0] === 'O' && board[1] === 'O' && board[2] === 'O' ||
+    board[3] === 'O' && board[4] === 'O' && board[5] === 'O' ||
+    board[6] === 'O' && board[7] === 'O' && board[8] === 'O' ||
+    board[0] === 'O' && board[3] === 'O' && board[6] === 'O' ||
+    board[1] === 'O' && board[4] === 'O' && board[7] === 'O' ||
+    board[2] === 'O' && board[5] === 'O' && board[8] === 'O' ||
+    board[0] === 'O' && board[4] === 'O' && board[8] === 'O' ||
+    board[2] === 'O' && board[4] === 'O' && board[6] === 'O'
       ) {
         console.log('O wins!!');
         return 'winO';
       } else if (
-    board[0] !== '' && board[1] !== '' && board[2] !== ''
-    && board[3] !== '' && board[4] !== '' && board[5] !== ''
-    && board[6] !== '' && board[7] !== '' && board[8] !== ''
+    board[0] !== '' && board[1] !== '' && board[2] !== '' &&
+    board[3] !== '' && board[4] !== '' && board[5] !== '' &&
+    board[6] !== '' && board[7] !== '' && board[8] !== ''
       ) {
         console.log('CATs game');
         return 'cat';
